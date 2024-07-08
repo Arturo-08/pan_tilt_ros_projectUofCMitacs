6d019331fd95e8fc3401bccc9531d39a6d0f4aeb
Carlos Arturo Buitrago Zamudio <cbuitragoz@unal.edu.co>
2024-07-08 10:43:03 -0600

    Setting inputs details

--- pan_tilt_driver/nodes/continuous_pan_tilt_control.py
+++ pan_tilt_driver/nodes/continuous_pan_tilt_control.py
@@ -3,7 +3,7 @@
 import rospy
 from pan_tilt_msgs.msg import PanTiltCmdDeg
 
-def continuous_movement(yaw, speed, pitch_min, pitch_max, rate_hz=10):
+def continuous_movement(yaw, speed, pitch_min, pitch_max, rate_hz=20):
     pub = rospy.Publisher('/pan_tilt_cmd_deg', PanTiltCmdDeg, queue_size=10)
     rospy.init_node('continuous_pan_tilt_control', anonymous=True)
     rate = rospy.Rate(rate_hz)  # Rate in Hz
@@ -17,7 +17,7 @@ def continuous_movement(yaw, speed, pitch_min, pitch_max, rate_hz=10):
         cmd.pitch = pitch
         cmd.speed = speed
         
-        rospy.loginfo("Moviendo a Yaw: {}, Pitch: {}, Speed: {}".format(yaw, pitch, speed))
+        rospy.loginfo("Current Yaw: {}, Pitch: {}, Speed: {}".format(yaw, pitch, speed))
         pub.publish(cmd)
 
         pitch += direction * speed / rate_hz
@@ -32,10 +32,10 @@ def continuous_movement(yaw, speed, pitch_min, pitch_max, rate_hz=10):
 
 if __name__ == '__main__':
     try:
-        yaw = float(input("yaw (-60 to 60): "))
-        speed = float(input("Ingrese el valor de speed (1 to 30): "))
-        pitch_min = float(input("Ingrese el valor de pitch mínimo (minimun -60): "))
-        pitch_max = float(input("Ingrese el valor de pitch máximo (maximum 60): "))
+        yaw = float(input("Input the yaw value (-55 to 55): "))
+        speed = float(input("Input the speed value (1 to 30): "))
+        pitch_min = float(input("Input minimum pitch value (minimun -60): "))
+        pitch_max = float(input("Input maximum pitch value (maximum 60): "))
         continuous_movement(yaw, speed, pitch_min, pitch_max)
     except rospy.ROSInterruptException:
         pass
